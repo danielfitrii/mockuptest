af4923bf0573a7741076fccb108970e164805127
danielftrii <daienlsocial@gmail.com>
2025-06-07 10:14:22 +0800

    Implemented communication from InputFragment to QuoteFragment via MainActivity

--- app/src/main/java/com/example/mockuptest/MainActivity.kt
+++ app/src/main/java/com/example/mockuptest/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.mockuptest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), InputFragment.InputListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +16,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    override fun onQuoteSubmit(quoteText: String, fontSize: Int) {
+        Log.d("MainActivity", "onQuoteSubmit called - Quote: $quoteText, Size: $fontSize")
+        val fragment = supportFragmentManager
+            .findFragmentById(R.id.quote_fragment) as? QuoteFragment
+        if (fragment == null) {
+            Log.e("MainActivity", "QuoteFragment not found!")
+        } else {
+            fragment.updateQuote(quoteText, fontSize)
         }
     }
 }
